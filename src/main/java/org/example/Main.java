1b81644ed0ea5c25ce9076993cf2655dd5c7e719
kaynak <kaynakhasbi1@gmail.com>
2023-07-18 16:09:49 +0200

    final keywords used in order to not change values accidentally

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Player initiator = new Player();
-        Player receiver = new Player();
-        Scanner input = new Scanner(System.in);
+        final Player initiator = new Player();
+        final Player receiver = new Player();
+        final Scanner input = new Scanner(System.in);
 
         int counter = 1;
         do {
             System.out.print("Please enter your " + counter + ". message: ");
-            String message = input.nextLine();
+            final String message = input.nextLine();
             initiator.sendMessage(receiver, message);
-            String reply = receiver.getNextReceivedMessage();
+            final String reply = receiver.getNextReceivedMessage();
             System.out.println("Message received: " + reply);
             counter++;
-            int leftMessages = 11 - counter;
+            final int leftMessages = 11 - counter;
             if (leftMessages == 0) {
                 System.out.println("You cannot send more messages. The program is shutting down. See you soon!");
             } else {
